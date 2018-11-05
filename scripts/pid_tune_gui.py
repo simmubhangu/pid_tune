45488575526fb22f3cd8bfbfdc2d9b013b2fdfc2
Fayyaz <fayyazpocker@gmail.com>
2018-11-05 23:50:50 +0530

    added yaw UI

--- scripts/pid_tune_gui.py
+++ scripts/pid_tune_gui.py
@@ -105,44 +105,46 @@ class Altitude():
 		# pid_value.Kp_z = scale3.get()
 		self.send_pid_altitude.publish(self.pid_value_altitude)
 		# print scale.get(), scale1.get(), scale2.get()
-		print "send",self.pid_value_altitude
-		print "\n"
+		# print "send",self.pid_value_altitude
+		# print "\n"
 
-# class Yaw():
-# 	def __init__(self):
-# 		self.send_pid_yaw = rospy.Publisher('/pid_tuning_yaw', PidTune, queue_size=10)
-# 		self.pid_value_yaw = PidTune()
+class Yaw():
+	def __init__(self):
+		self.send_pid_yaw = rospy.Publisher('/pid_tuning_yaw', PidTune, queue_size=10)
+		self.pid_value_yaw = PidTune()
 
-# 		self.root = Tk()
-# 		self.root.title("Yaw")
+		self.root = Tk()
+		self.root.title("Yaw")
 
-# 		self.scale = Scale(self.root, orient='horizontal', from_=0, to=300, label= 'Kp',width = "50", length = "500",troughcolor="red")
-# 		self.scale1 =Scale(self.root, orient='horizontal', from_=0, to=1000, label= 'Ki',width = "50", length = "500",troughcolor="green")
-# 		self.scale2 =Scale(self.root, orient='horizontal', from_=0, to=100, label= 'Kd',width = "50", length = "500", troughcolor="blue")
-# 		# scale3 =Scale(root, orient='horizontal', from_=0, to=300, label= 'Kp_z',width = "50", length = "500", troughcolor="gray")
-# 		self.scale.pack()
-# 		self.scale1.pack()
-# 		self.scale2.pack()
-# 		# scale3.pack()
+		self.scale = Scale(self.root, orient='horizontal', from_=0, to=5000, label= 'Kp',width = "50", length = "500",troughcolor="red")
+		self.scale1 =Scale(self.root, orient='horizontal', from_=0, to=5000, label= 'Ki',width = "50", length = "500",troughcolor="green")
+		self.scale2 =Scale(self.root, orient='horizontal', from_=0, to=5000, label= 'Kd',width = "50", length = "500", troughcolor="blue")
+		# scale3 =Scale(root, orient='horizontal', from_=0, to=300, label= 'Kp_z',width = "50", length = "500", troughcolor="gray")
+		self.scale.pack()
+		self.scale1.pack()
+		self.scale2.pack()
+		# scale3.pack()
 
-# 		Button(self.root, text='set_value', command=self.set_value).pack()
+		Button(self.root, text='set_value', command=self.set_value).pack()
 
-# 	#root.mainloop()
+	#root.mainloop()
 
-# 	def set_value(self):
+	def set_value(self):
 
-# 		self.pid_value_yaw.Kp = self.scale.get()
-# 		self.pid_value_yaw.Ki = self.scale1.get()
-# 		self.pid_value_yaw.Kd = self.scale2.get()
-# 		# pid_value.Kp_z = scale3.get()
-# 		self.send_pid_yaw.publish(self.pid_value_yaw)
-# 		# print scale.get(), scale1.get(), scale2.get()
-# 		print "send",self.pid_value_yaw
-# 		print "\n"
+		self.pid_value_yaw.Kp = self.scale.get()
+		self.pid_value_yaw.Ki = self.scale1.get()
+		self.pid_value_yaw.Kd = self.scale2.get()
+		# pid_value.Kp_z = scale3.get()
+		self.send_pid_yaw.publish(self.pid_value_yaw)
+		# print scale.get(), scale1.get(), scale2.get()
+		# print "send",self.pid_value_yaw
+		# print "\n"
+
 
 pitch = Pitch()
 roll = Roll()
 altitude = Altitude()
+yaw = Yaw()
 
 def quit(event):
 	pitch.root.quit()
@@ -158,5 +160,6 @@ if __name__ == '__main__':
 	pitch.root.bind('<Control-c>',quit)
 	roll.root.bind('<Control-c>',quit)
 	altitude.root.bind('<Control-c>',quit)
+	yaw.root.bind('<Control-c>',quit)
 	pitch.root.mainloop()
 	# roll = Roll()
